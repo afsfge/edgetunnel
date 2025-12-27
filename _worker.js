3f7b20bd44884ee5d8cab22ec6d77c4df050b8b4
稻草人 <gecp@vip.qq.com>
2025-12-27 22:39:35 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -776,7 +776,7 @@ function surge(content, url, config_JSON) {
     const 每行内容 = content.includes('\r\n') ? content.split('\r\n') : content.split('\n');
 
     let 输出内容 = "";
-	let realSurgePath = config_JSON.启用0RTT ? config_JSON.PATH + '?ed=2560' : config_JSON.PATH;
+	let realSurgePath = config_JSON.启用0RTT ?  '/?ed=2560&'+config_JSON.反代.PROXYIP+':443' : config_JSON.PATH;
     for (let x of 每行内容) {
         if (x.includes('= tro' + 'jan,')) {
             const host = x.split("sni=")[1].split(",")[0];
@@ -1578,4 +1578,5 @@ async function html1101(host, 访问IP) {
 </body>
 </html>`;
 }
+
 
